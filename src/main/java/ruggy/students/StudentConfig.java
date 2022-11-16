e0d31dd9a8452fee06ea04d279f823a7f5e1fd6d
Jaston Ruggy <jastonruggy@icloud.com>
2022-11-16 15:22:00 +0300

    sdfg

---
+++ src/main/java/ruggy/students/StudentConfig.java
@@ -0,0 +1,35 @@
+package ruggy.students;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student mariam = new Student(
+                    "Mariam",
+                    "jastonruggy@icloud.com",
+                    LocalDate.of(2000, Month.JANUARY, 5),
+                    21);
+
+            Student alex = new Student(
+                    "Alex",
+                    "kaka@icloud.com",
+                    LocalDate.of(1999, Month.JANUARY, 5),
+                    21);
+
+                    repository.saveAll(
+                        List.of(mariam,alex)
+                    );
+        };
+    }
+}
--- src/main/java/ruggy/students/StudentService.java
+++ src/main/java/ruggy/students/StudentService.java
@@ -5,20 +5,21 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
+
+	private final StudentRepository studentRepository;
+
+	@Autowired
+	public StudentService(StudentRepository studentRepository){
+		this.studentRepository = studentRepository;
+	}
+
     public List<Student> hello() {
-		return List.of(
-			new Student(
-				1L,
-				"Mariam",
-				"jastonruggy@icloud.com",
-				LocalDate.of(2000, Month.JANUARY, 5),
-				21
-			)
-		);
+		return studentRepository.findAll();
 	}
 
     public List<Student> getStudents() {
